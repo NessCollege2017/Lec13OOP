13f242464d450e514e39192c6c0f253252afc780
nesscollege2017 <nesscollege2017@gmail.com>
2017-02-17 12:58:12 +0200

    ArrayList add, remove, size, get -  our first StudentList

--- src/edu/tomer/java/Main.java
+++ src/edu/tomer/java/Main.java
@@ -1,34 +1,41 @@
 package edu.tomer.java;
 
 import java.time.LocalDateTime;
-import java.util.Date;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-//        // instantiate a student:
-//        Student moshe = new Student(
-//                "Moshe", "Doe", "03030405055",
-//                "Moshe@gmail.com", "050-7123012", "Mesharerim 4"
-//        );
-//
-//        //moshe.print();
-//        moshe.sayYourName();
+        // instantiate a student:
+        Student moshe = new Student(
+                "Moshe", "Doe", "03030405055",
+                "Moshe@gmail.com", "050-7123012", "Mesharerim 4"
+        );
 
+        //this is a list of students:
+        //init the list:
+        ArrayList<Student> studentList = new ArrayList<>();
 
-        LocalDateTime now = LocalDateTime.now();
+        //add an item to the list:
+        studentList.add(moshe);
+        studentList.add(new Student());
 
+        //get an item from the list:
+        //get an item by index:
+        studentList.get(0);
 
+        //whats the size of the list:
+        int listSize = studentList.size();
+        for (int i = 0; i < studentList.size(); i++) {
+            Student s = studentList.get(i);
+            s.print();
+        }
 
-//
-//        Student s = new Student();
-//        s.print();
-
-       // moshe.print();
-
-
+        //remove an item from the list:
+        //remove by index:
+        studentList.remove(1);
+        //remove a specific object:
+        studentList.remove(moshe);
 
     }
 }
---
+++ src/edu/tomer/java/StudentPhoneBook.java
@@ -0,0 +1,32 @@
+package edu.tomer.java;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Android2017 on 17/02/2017.
+ */
+//Alphon:
+public class StudentPhoneBook {
+    //init the list:
+    ArrayList<Student> studentList = new ArrayList<>();
+
+    void addStudent(Student s){
+
+    }
+
+    void addStudent(){
+        Student s = new Student();//IO!!!
+    }
+
+    void removeStudent(int index){
+
+    }
+
+    void showStudent(int index){
+
+    }
+
+    void showAllStudents(){
+
+    }
+}
